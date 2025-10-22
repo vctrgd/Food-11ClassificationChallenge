e3c8aecdcd3701573972102be12863c96bb655d0
Rigaud Victor <vrigaud@3D-LINUX-07.localdomain>
2025-10-22 09:23:36 +0200

    ajout de la préparation des données et update gitignore
     Validateur : vctrgd <rigaudvictor13@gmail.com>

---
+++ prepateData.m
@@ -0,0 +1,31 @@
+%% Extract the FOOD11 images into arrays
+disp('Preparing FOOD11 data...');
+
+datasetPath = fullfile(pwd, 'train');  
+
+%disp(datasetPath)
+imds = imageDatastore(datasetPath, ...
+    'IncludeSubfolders', true, ...
+    'LabelSource', 'foldernames');
+
+disp("Nombre total d'images : " + numel(imds.Files));
+disp("Classes trouvées :");
+disp(categories(imds.Labels));
+
+% 4. Équilibrage des classes (optionnel)
+%[imdsBalanced, ~] = splitEachLabel(imds, min(countEachLabel(imds).Count), 'randomized');
+
+% 5. Division en train / validation (80/20)
+[imdsTrain, imdsVal] = splitEachLabel(imds, 0.8, 'randomized');
+
+% 6. Redimensionnement des images à une taille fixe (par exemple : 224x224)
+inputSize = [224 224];  % à adapter selon le modèle utilisé plus tard
+
+augmentedTrain = augmentedImageDatastore(inputSize, imdsTrain);
+augmentedVal   = augmentedImageDatastore(inputSize, imdsVal);
+
+% 7. Sauvegarde des objets préparés
+save('prepared_data.mat', 'augmentedTrain', 'augmentedVal');
+clear imds imdsTrain imdsVal imgDataTest imgDataTrain datasetPath labelsTrain
+
+disp("Préparation des données terminée.");
