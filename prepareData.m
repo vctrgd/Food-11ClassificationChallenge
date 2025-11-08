4d1032a2d12c11c271ac2d18e5f9770a5aa53a93
Jean-Baptiste SIMON <72874867+Jbsimon05@users.noreply.github.com>
2025-11-08 15:18:08 +0100

    MEF prepareData

--- prepareData.m
+++ prepareData.m
@@ -1,34 +1,33 @@
 function [augmentedTrain, augmentedVal] = prepareData()
-%PREPAREDATA Prépare les données FOOD11 pour l'entraînement et la validation.
+% Prépare les données FOOD11 pour l'entraînement et la validation
 %
 % Retourne :
-%   augmentedTrain : datastore d’images augmentées pour l'entraînement
-%   augmentedVal   : datastore d’images augmentées pour la validation
+%   augmentedTrain  :   datastore d’images augmentées pour l'entraînement
+%   augmentedVal    :   datastore d’images augmentées pour la validation
 
-    disp('Preparing FOOD11 data...');
-
-    datasetPath = fullfile(pwd, 'train');  
-
+    %% Chargement des images
+    disp('Préparation des données FOOD11...');
+    disp();
+    datasetPath = fullfile(pwd, 'train');
     imds = imageDatastore(datasetPath, ...
         'IncludeSubfolders', true, ...
         'LabelSource', 'foldernames');
-
     disp("Nombre total d'images : " + numel(imds.Files));
-    disp("Classes trouvées :");
-    disp(categories(imds.Labels));
+    disp("Classes trouvées :" + categories(imds.Labels));
+    disp();
 
-    % Division en train / validation
+    %% Division en train / validation
     [imdsTrain, imdsVal] = splitEachLabel(imds, 0.8, 'randomized');
 
-    % Taille d’entrée (RegNet/ResNet → 224x224)
+    %% Taille d’entrée (RegNet/ResNet → 224x224)
     inputSize = [224 224];
 
-    % Création des datastores augmentés
+    %% Création des datastores augmentés
     augmentedTrain = augmentedImageDatastore(inputSize, imdsTrain);
     augmentedVal   = augmentedImageDatastore(inputSize, imdsVal);
 
-    % Sauvegarde optionnelle
+    %% Sauvegarde
     save('prepared_data.mat', 'augmentedTrain', 'augmentedVal');
-
     disp("Préparation des données terminée.");
+
 end
